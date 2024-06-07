da9757423d0ba40d42e9f44c07c0887b5db31971
Marcel Kempel <marcelkempel127@gmail.com>
2024-06-07 08:51:00 +0200

    Übernahme von Quellcode einer anderen Datei

--- lib/main.dart
+++ lib/main.dart
@@ -1,126 +1,268 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const PortfolioApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class PortfolioApp extends StatelessWidget {
+  const PortfolioApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: "Portfolio von Marcel Kempel",
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          scaffoldBackgroundColor: const Color.fromARGB(100, 215, 215, 215)),
+      home: const PortfolioPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class PortfolioPage extends StatelessWidget {
+  const PortfolioPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Portfolio"),
+        backgroundColor: const Color.fromARGB(255, 128, 186, 36),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: const SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          children: [
+            Ueberschrift(),
+            UeberMich(),
+            Lebenslauf(),
+            Faehigkeiten(),
+            Fusszeile(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class Ueberschrift extends StatelessWidget {
+  const Ueberschrift({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      color: const Color.fromARGB(255, 128, 186, 36),
+      child: const Column(
+        children: [
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: NetworkImage(
+                "https://www.museumize.com/cdn/shop/products/42d69af387d6044850ae79ca5e5acb5d_1024x1024@2x.jpg?v=1582156263"),
+          ),
+          SizedBox(height: 10),
+          Text(
+            "Marcel Kempel",
+            style: TextStyle(fontSize: 24, color: Colors.white),
+          ),
+          Text(
+            "THM Student",
+            style: TextStyle(fontSize: 18, color: Colors.white70),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class UeberMich extends StatelessWidget {
+  const UeberMich({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "About Me",
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 10),
+          Text(
+            "Still a Beginner but I\'m willing to learn",
+            style: TextStyle(fontSize: 16),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Lebenslauf extends StatelessWidget {
+  const Lebenslauf({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Lebenslauf",
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 10),
+          ExperienceItem(
+            title: "THM",
+            description: "Studium mit kleinen Projekten",
+          ),
+          ExperienceItem(
+            title: 'Bundeswehr',
+            description: "Aufklärer bei der Bundeswehr",
+          ),
+          ExperienceItem(
+            title: "Abitur",
+            description: "Abitur an dem GSG Winterberg",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ExperienceItem extends StatelessWidget {
+  final String title;
+
+  final String description;
+
+  const ExperienceItem(
+      {super.key, required this.title, required this.description});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 5),
+          Text(
+            description,
+            style: const TextStyle(fontSize: 16),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class EducationItem extends StatelessWidget {
+  final String title;
+
+  const EducationItem({
+    super.key,
+    required this.title,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [],
+      ),
+    );
+  }
+}
+
+class Faehigkeiten extends StatelessWidget {
+  const Faehigkeiten({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Fähigkeiten',
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 10),
+          Wrap(
+            spacing: 10,
+            runSpacing: 10,
+            children: [
+              SkillChip(skill: 'Java'),
+              SkillChip(skill: 'Flutter'),
+              SkillChip(skill: 'Dart'),
+              SkillChip(skill: 'SQL'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SkillChip extends StatelessWidget {
+  final String skill;
+
+  const SkillChip({super.key, required this.skill});
+
+  @override
+  Widget build(BuildContext context) {
+    return Chip(
+      label: Text(skill),
+      backgroundColor: const Color.fromARGB(255, 128, 186, 36),
+    );
+  }
+}
+
+class Fusszeile extends StatelessWidget {
+  const Fusszeile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      color: const Color.fromARGB(255, 128, 186, 36),
+      child: Column(
+        children: [
+          const Text(
+            'Marcel Kempel',
+            style: TextStyle(color: Colors.white),
+          ),
+          const Text(
+            'Contact me:',
+            style: TextStyle(color: Colors.white70),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.local_phone_sharp),
+                color: Colors.white,
+                tooltip: "+4915789048153",
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: const Icon(Icons.email),
+                color: Colors.white,
+                tooltip: "Marcelkempel127@gmail.com",
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
